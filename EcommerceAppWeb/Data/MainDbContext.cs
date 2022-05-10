e614ff4815ff48a6c86d98cc5b64f6a8824a95a4
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-05-10 11:34:23 +0200

    Add project files.

---
+++ EcommerceAppWeb/Data/MainDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EcommerceAppWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAppWeb.Data
+{
+    public class MainDbContext :DbContext 
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ EcommerceAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommerceAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
