2d3b0004613889f68d9a4dcbe9d5db3b460efe3b
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-05-11 14:10:19 +0200

    Added CRUD controllers to Category

--- EcommerceAppWeb/Controllers/CategoryController.cs
+++ EcommerceAppWeb/Controllers/CategoryController.cs
@@ -73,5 +73,35 @@ namespace EcommerceAppWeb.Controllers
             }
             return View(obj);
         }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) 
+            { 
+                return NotFound(); 
+            }
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
     }
 }
