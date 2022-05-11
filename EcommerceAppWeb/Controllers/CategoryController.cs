0bf459c952accb0e78e1c6d23a32166310cbb9e9
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-05-11 11:59:18 +0200

    Added create

--- EcommerceAppWeb/Controllers/CategoryController.cs
+++ EcommerceAppWeb/Controllers/CategoryController.cs
@@ -30,11 +30,44 @@ namespace EcommerceAppWeb.Controllers
         {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("CustomError", "Name and DisplayOrder can not be the same");
+                ModelState.AddModelError("CustomError", "Name and Display Order can not be the same.");
             }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Name and Display Order can not be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
