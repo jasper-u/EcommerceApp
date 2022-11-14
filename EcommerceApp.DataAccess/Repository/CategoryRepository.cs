5aeb48b07031b3c3c30dc2a61cd32a6dc790f921
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-11-14 12:50:54 +0100

    Added CategoryRepository

---
+++ EcommerceApp.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceApp.DataAccess.Repository.IRepository;
+using EcommerceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private MainDbContext _db;
+
+        public CategoryRepository(MainDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
