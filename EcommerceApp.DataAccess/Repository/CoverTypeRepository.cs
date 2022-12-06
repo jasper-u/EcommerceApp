48f1322a90c7a903e3c42f40313479b79ae6f387
Jasper Uitvlugt <jasper.uitvlugt.bee@outlook.com>
2022-12-06 13:59:14 +0100

    Added CoverTypeRepository

---
+++ EcommerceApp.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceApp.DataAccess.Repository.IRepository;
+using EcommerceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceApp.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private MainDbContext _db;
+
+        public CoverTypeRepository(MainDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
--- EcommerceApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ EcommerceApp.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace EcommerceApp.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
 
         void Save();
     }
